0cb8a2141c417a6922ad6259b1ad466ca0c7e774
Aaron Hewlett <aaron.hewlett@gmail.com>
2018-04-25 21:55:53 -0230

    Created a new project with some simple unit tests.

---
+++ VocableParser.Tests/WordStructureTests.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VocableParser.Tests
+{
+    [TestClass]
+    public class WordStructureTests
+    {
+        [TestMethod]
+        public void WordStructure_OnlyConstantComponents_ValidResult()
+        {
+            // arrange
+            string[] expectedResults = { "ab", "ac", "ib", "ic" };
+            WordStructure structure = WordStructure.Parse("VC");
+            structure.FillComponents('V', "a", "i");
+            structure.FillComponents('C', "b", "c");
+            // act
+            var results = structure.BuildWords();
+            // assert
+            Assert.IsTrue(results.All(expectedResults.AsEnumerable().Contains), "Unexpected results using only constant components.");
+            Assert.IsTrue(results.Count() == expectedResults.Count(), "Count of result does not match expected.");
+        }
+
+        [TestMethod]
+        public void WordStructure_ConstantAndOptionalComponents_ValidResult()
+        {
+            // arrange
+            string[] expectedResults = { "ab", "ac", "ib", "ic", "aba", "abi", "aca", "aci", "iba", "ibi", "ica", "ici" };
+            WordStructure structure = WordStructure.Parse("VC(V)");
+            structure.FillComponents('V', "a", "i");
+            structure.FillComponents('C', "b", "c");
+            // act
+            var results = structure.BuildWords();
+            // assert
+            Assert.IsTrue(results.All(expectedResults.AsEnumerable().Contains), "Unexpected results using constant and optional components.");
+            Assert.IsTrue(results.Count() == expectedResults.Count(), "Count of result does not match expected.");
+        }
+
+        [TestMethod]
+        public void WordStructure_OnlyOptionalComponents_ValidResult()
+        {
+            // arrange
+            string[] expectedResults = { "ab", "a", "b" };
+            WordStructure structure = WordStructure.Parse("(V)(C)");
+            structure.FillComponents('V', "a");
+            structure.FillComponents('C', "b");
+            // act
+            var results = structure.BuildWords();
+            // assert
+            Assert.IsTrue(results.All(expectedResults.AsEnumerable().Contains), "Unexpected results using only optional components.");
+            Assert.IsTrue(results.Count() == expectedResults.Count(), "Count of result does not match expected.");
+        }
+    }
+}
